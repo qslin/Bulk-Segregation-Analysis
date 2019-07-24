e75d52d9034158dd6a90bb5310ed3019f23d1150
Qiaoshan Lin <qiaoshan.lin@uconn.edu>
2019-07-24 19:48:00 -0400

    annotate candidates without filtering

--- QSBSA.sh
+++ QSBSA.sh
@@ -33,7 +33,7 @@ usage()
     echo "    -b | --bsa    To turn on the bulk segregation analysis with another genome to map to. Example: -b /home/CAM/qlin/SL9_Genome/version/v2.0/SL9_consensus.fa"
     echo "    -l | --library    To set the library for mutant SNPs comparing. Default: /home/CAM/qlin/BSA/LF10_vcf.txt"
     echo "    -s | --step	To restart with a certain step(trim/align/call-SNPs/filter/annotate). The results from previous run will be overwritten. Example: -s align"
-    echo "    -g | --gtf 	To provide the gtf file for annotation of SNPs or set this option to be unavailable(n). Default: /home/CAM/qlin/LF10_Genome/14_isoform_filtering/t4/LF10.gtf"
+    echo "    -g | --gtf 	To provide the gtf file for annotation of SNPs or set this option to be unavailable(n). Default: /home/CAM/qlin/resource/LF10/LF10g_v2.0.gtf"
     echo "    -c | --codingseq  To provide the coding sequence for annotation of SNPs in case no gtf file exists. Default: /home/CAM/qlin/resource/LF10/LF10T_v1.2.fa"
     echo "    -h | --help	To see the usage."
     echo "Notes:"
@@ -69,7 +69,7 @@ do
 	bamarray=("${bamarray[@]}" $n"_reads.bam")
 done
 samtools merge reads.bam ${bamarray[@]}
-samtools sort -o reads.sort.bam -T samtmp -O bam -@ 8 reads.bam
+samtools sort -@ 8 -o reads.sort.bam -T samtmp -O bam -@ 8 reads.bam
 rm *_reads.sam *_reads.bam reads.bam
 bamtools filter -in reads.sort.bam -out reads.sort.filter1.bam -tag "XM:<3"
 samtools depth -aa -q 20 -Q 20 reads.sort.filter1.bam > depth.txt
@@ -81,8 +81,8 @@ call()
 samtools stats -c 1,1000,1 reads.sort.filter1.bam > stats.txt
 abn=`grep '^SN' stats.txt |cut -f2-|grep '^bases mapped (cigar)'|cut -f2`
 avg=`grep '^COV' stats.txt |cut -f3-|sort -nrk2,2|head -1|cut -f1`
-ulim=`expr $avg \\* 3`
-blim=`expr $avg \\/ 3`
+ulim=`echo "$(($avg*25/10))"`
+blim=`echo "$(($avg*10/25))"`
 bcftools mpileup -Ou --threads 8 -Q 20 -q 20 -f $ref reads.sort.filter1.bam | bcftools call -Ou --threads 8 -mv | bcftools filter -e '%QUAL<20' > snp0.vcf
 snpcount0=`grep -c -v '^#' snp0.vcf`
 echo Round 0: $snpcount0 SNPs
@@ -92,21 +92,28 @@ bcftools filter -e $covflt snp0.vcf  > snp1.vcf
 echo Filter SNPs by coverage: $covflt
 snpcount1=`grep -c -v '^#' snp1.vcf`
 echo Round 1: $snpcount1 SNPs
+
 frqflt="(DP4[2]+DP4[3])/(DP[0]+DP[1]+DP4[2]+DP4[3])<0.25"
 bcftools filter -e $frqflt snp1.vcf > snp2.vcf
+bcftools norm -f $ref snp2.vcf -Ov -o snp2.norm.vcf
+mv snp2.norm.vcf snp2.vcf
 echo Filter SNPs by alt frequency
 snpcount2=`grep -c -v '^#' snp2.vcf`
 echo Round 2: $snpcount2 SNPs
-bcftools filter -e '(DP4[0]+DP4[1])/(DP4[0]+DP4[1]+DP4[2]+DP4[3]) > 0.05' snp2.vcf > snp3.vcf
+
+awk '{split($10, a, ":");split(a[2], b, ",");if (b[1]>40&&b[2]>20&&b[3]==0) print}' snp2.vcf > snp3.vcf
+grep '^#' snp2.vcf |cat - snp3.vcf > tmp
+mv tmp snp3.vcf
 echo Filter SNPs by homozygousity
 snpcount3=`grep -c -v '^#' snp3.vcf`
 echo Round 3: $snpcount3 SNPs
+
 echo Finish SNP calling and filtering
-newvcf=`pwd` 
-echo "$newvcf/snp2.vcf">> /home/CAM/qlin/BSA/vcf.txt
-sort /home/CAM/qlin/BSA/vcf.txt |uniq > /home/CAM/qlin/BSA/newvcf.txt
-mv /home/CAM/qlin/BSA/newvcf.txt /home/CAM/qlin/BSA/vcf.txt
-echo VCF library updated at /home/CAM/qlin/BSA/vcf.txt
+#newvcf=`pwd` 
+#echo "$newvcf/snp2.vcf">> /home/CAM/qlin/BSA/vcf.txt
+#sort /home/CAM/qlin/BSA/vcf.txt |uniq > /home/CAM/qlin/BSA/newvcf.txt
+#mv /home/CAM/qlin/BSA/newvcf.txt /home/CAM/qlin/BSA/vcf.txt
+#echo VCF library updated at /home/CAM/qlin/BSA/vcf.txt
 }
 
 filter()
@@ -118,17 +125,43 @@ echo Finish SNPs filtering
 annotate()
 {
 if [ "$gtf" != "n" ]; then
-	awk '{print "\$1~/"$1"/&&\$4<="$2"&&\$5>="$2"&&\$3~/exon|intron/{print "NR"\"\\\\t\"\$0}"}' SNP.vcf > cmd.txt
+	awk '{print "\$1~/"$1"/&&\$4<="$2"&&\$5>="$2"&&\$3~/CDS|intron/{print "NR"\"\\\\t\"\$0}"}' SNP.vcf > cmd.txt
 	pos=()
 	while read line; do   pos+=("$line");   done < cmd.txt
-	for p in "${pos[@]}"
+	snp=()
+	while read line; do   snp+=("$line");   done < SNP.vcf
+	for p in "${!pos[@]}"
 	do
-		awk "$p" $gtf >> SNP.gtf
+		awk "${pos[$p]}" $gtf |cut -f4-6,10 |tr "\n" "\t"> snptmp
+		awk "${pos[$p]}" $gtf > snptmp2
+		feature=`awk "${pos[$p]}" $gtf |cut -f4`
+		if [[ -s "snptmp" && $feature =~ "CDS" ]];then 
+			snptmp=()
+			while read line; do   snptmp+=("$line");   done < snptmp2
+			for s in "${snptmp[@]}"
+			do
+				echo "$s"|cut -f4-6,10 |tr "\n" "\t" > tmp
+				feature=`echo $s|awk '{print $4}'`
+				if [[ $feature =~ "CDS" ]];then
+					startpos=`echo $s|awk '{print $5}'`
+		        	        startframe=`echo $s|awk '{print $9}'`
+        			        snppos=`echo "${snp[$p]}"|awk '{print $2}'`
+        			        snpframe="$(( (($snppos-$startpos)%3+$startframe)%3 ))"
+                			snpchr=`echo "${snp[$p]}"|awk '{print $1}'`
+                			normstart=`echo "$(($snppos-$snpframe))"`
+                			normend=`echo "$(($snppos-$snpframe+2))"`
+               				samtools faidx $ref $snpchr:$normstart-$normend | tail -n +2 |awk -v frame="$snpframe" '{split($1,a,"");b=tolower(a[frame+1]);a[frame+1]=b;print a[1]a[2]a[3]"\t"frame}'|tr "\n" "\t"> codontmp
+					echo "${snp[$p]}"|tr "\n" "\t"|cat - tmp codontmp |sed 's/$/\n/' >> SNP.annotate.vcf.tmp
+				else
+					echo "${snp[$p]}"|tr "\n" "\t"|cat - tmp |sed 's/$/\n/' >> SNP.annotate.vcf.tmp
+				fi
+			done
+		else
+			echo "${snp[$p]}"|tr "\n" "\t"|cat - snptmp |sed 's/$/\n/' >> SNP.annotate.vcf.tmp
+		fi
 	done
-	sort -k1b,1 SNP.gtf > SNP.gtf.tmp
-	awk '{print NR"\t"$0}' SNP.vcf |sort -k1b,1 > SNP.vcf.tmp
-	join -j 1 SNP.vcf.tmp SNP.gtf.tmp |sort -k1b,1 -n | cut -f2- -d " " > SNP.annotate.vcf
-	rm cmd.txt SNP.vcf.tmp SNP.gtf.tmp SNP.gtf
+	rm cmd.txt snptmp tmp snptmp2 codontmp
+	mv SNP.annotate.vcf.tmp SNP.annotate.vcf
 	cp SNP.annotate.vcf ../candidates.annotate.vcf
 else
 	awk '{if($2>1000)print $1":"$2-1000"-"$2+1000;else print $1":1-"$2+1000}' SNP.vcf > pos.txt
@@ -152,30 +185,24 @@ echo Finish annotation
 bsa_annotate()
 {
 awk '$3>0{print $1"\t"$2"\t"$2+20000}' SNP.txt |tail -n +2 > tmp.bed
-bedtools merge -i tmp.bed |awk '$3-$2>80000{print}'|bedtools merge -i - -d 200000 |awk '{print $1":"$2"-"$3}'> high_count_regions.txt
+bedtools merge -i tmp.bed |awk '$3-$2>80000{print}'|bedtools merge -i - -d 200000 |awk '{print $1"\t"$2"\t"$3}'> high_count_regions.bed
 rm tmp.bed
-array=()
-while read line; do   array+=("$line");   done < high_count_regions.txt
-for n in "${array[@]}"
-do
-	samtools faidx $ref $n >> high_count_regions.fa
-done
-minimap2 $oldref high_count_regions.fa > ../high_count_regions.paf
-rm high_count_regions.fa
 cd ..
-awk '$2*0.9<$11{print $6"\t"$8"\t"$9}' high_count_regions.paf > high_count_regions.bed
+awk '{print $1"\t"$2"\t"$2}' SNP.vcf |bedtools intersect -wb -a bsatmp/high_count_regions.bed -b - |awk '{print $4"\t"$5}' > candidates.txt
 if [ "$gtf" != "n" ]; then
-	awk '{print $1"\t"$2"\t"$2}' SNP.vcf |bedtools intersect -wb -a high_count_regions.bed -b - |awk '{print $4" "$5}' > candidates.txt
 	array=()
 	while read line; do   array+=("$line");   done < candidates.txt
+	cp SNP.annotate.vcf candidates.annotate.vcf
 	for n in "${array[@]}"
 	do
-		grep -P "$n" SNP.annotate.vcf >> candidates.annotate.vcf
+		chr=`echo $n|tr " " "\t"|cut -f1`
+		pos=`echo $n|tr " " "\t"|cut -f2`
+		awk -v chr="$chr" -v pos="$pos" '{if($1 ~ chr&&$2 ~ pos){print $0"\thigh_count_region"}else{print}}' candidates.annotate.vcf > tmp.annotate.vcf
+		mv tmp.annotate.vcf candidates.annotate.vcf 
 	done
 	mv candidates.annotate.vcf ../candidates.annotate.vcf
 else
-	awk '{print $1"\t"$2"\t"$2}' SNP.vcf |bedtools intersect -wb -a high_count_regions.bed -b - |awk '{print $4"\t"$5}' > candidates.vcf
-	awk '{if($2>1000)print $1":"$2-1000"-"$2+1000;else print $1":1-"$2+1000}' candidates.vcf > pos.txt
+	awk '{if($2>1000)print $1":"$2-1000"-"$2+1000;else print $1":1-"$2+1000}' candidates.txt > pos.txt
         pos=()
         while read line; do   pos+=("$line");   done < pos.txt
         for p in "${pos[@]}"
@@ -196,7 +223,7 @@ echo Finish annotation
 ##### Main
 
 lib=/home/CAM/qlin/BSA/LF10_vcf.txt
-gtf=/home/CAM/qlin/LF10_Genome/14_isoform_filtering/t4/LF10.gtf
+gtf=/home/CAM/qlin/resource/LF10/LF10g_v2.0.gtf
 cod=/home/CAM/qlin/resource/LF10/LF10T_v1.2.fa
 
 if [ $# -lt 4 ]; then 
@@ -298,9 +325,16 @@ case $step in
         "" ) ;&
         "trim" ) trim ;&
         "align" ) align ;&
-        "call-SNPs" ) call ;&
+        "call-SNPs" ) 
+		call 
+		newvcf=`pwd`
+		echo "$newvcf/snp2.vcf">> /home/CAM/qlin/BSA/vcf.txt
+		sort /home/CAM/qlin/BSA/vcf.txt |uniq > /home/CAM/qlin/BSA/newvcf.txt
+		mv /home/CAM/qlin/BSA/newvcf.txt /home/CAM/qlin/BSA/vcf.txt
+		echo VCF library updated at /home/CAM/qlin/BSA/vcf.txt
+		;&
         "filter" ) filter ;&
-		"annotate" ) annotate ;;
+	"annotate" ) annotate ;;
         *) echo "Error: no such a step"; exit 1 ;;
 esac
 
@@ -316,14 +350,16 @@ if [ "$bsaref" != "" ]; then
         "align" ) align ;&
         "call-SNPs" ) call ;&
         "filter" ) 
-			perl ~/scripts/QSBSA.pl -f=snp3.vcf -m=on -v=off -o=SNP 
-			cp SNP.eps ../../SNP.eps
-			echo Finish SNPs filtering
-			;&
-		"annotate" ) bsa_annotate ;;
+		perl ~/scripts/QSBSA.pl -f=snp3.vcf -m=on -v=off -o=SNP 
+		cp SNP.eps ../../SNP.eps
+		echo Finish SNPs filtering
+		;&
+	"annotate" ) bsa_annotate ;;
         *) echo "Error: no such a step"; exit 1 ;;
 	esac
-	
-	
 fi
+
+grep 'intron' ../candidates.annotate.vcf |awk '$2-$12<5||$13-$2<5{print}' |cut -f1-2,4-5,11- > ../check_first.vcf
+grep 'CDS' ../candidates.annotate.vcf |cut -f1-2,4-5,11- >> ../check_first.vcf
+
 
